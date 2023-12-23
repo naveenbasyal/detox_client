45860cd218d4c75dd75bca5f1a230ac4c482f440
Naveen Basyal <naveenbasyal.001@gmail.com>
2023-12-23 19:12:15 +0530

    optimised loading

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -7,31 +7,39 @@ import { useSelector } from "react-redux";
 
 const Main = () => {
   const { loading } = useSelector(({ dailyEntries }) => dailyEntries);
+  const { loading: userLoading } = useSelector((state) => state?.user);
+  console.log("loading", loading);
+  console.log("userLoading", userLoading);
+
   return (
     <section className="main mb-5">
-      <div className="container">
-        <CreateEntry />
+      {userLoading ? (
+        <MainLoader />
+      ) : (
+        <div className="container">
+          <CreateEntry />
 
-        <div className="row mt-4">
-          <div className="col-md-6">
-            <div className="card">
-              <div className="card-header">
-                <h2 className="mb-0">People's experiences</h2>
+          <div className="row mt-4">
+            <div className="col-md-6">
+              <div className="card">
+                <div className="card-header">
+                  <h2 className="mb-0">People's experiences</h2>
+                </div>
               </div>
+              <PublicEntries />
             </div>
-            <PublicEntries />
-          </div>
 
-          <div className="col-md-6">
-            <div className="card">
-              <div className="card-header">
-                <h2 className="mb-0">Challenges</h2>
+            <div className="col-md-6">
+              <div className="card">
+                <div className="card-header">
+                  <h2 className="mb-0">Challenges</h2>
+                </div>
               </div>
+              <Explore />
             </div>
-            <Explore />
           </div>
         </div>
-      </div>
+      )}
     </section>
   );
 };
